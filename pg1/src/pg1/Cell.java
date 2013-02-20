f362260a98ed0313e3fcfc641db19fe7014e0330
migulorama <migulorama@gmail.com>
2013-02-20 16:39:45 +0000

    Create last class project.

---
+++ pg1/src/pg1/Cell.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package pg1;
+
+/**
+ * @author miguel
+ *
+ */
+public class Cell<T> {
+	
+	public Cell(T val)
+	{
+		_value = val;
+	}
+	
+	public T GetValue() { return _value; }
+	public void SetValue(T val) { _value = val; }
+	
+	@Override
+	public String toString()
+	{
+		return _value.toString();
+	}
+	
+	private T _value;
+}
---
+++ pg1/src/pg1/Grid.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package pg1;
+
+import java.lang.reflect.Array;
+
+/**
+ * @author miguel
+ *
+ */
+public class Grid<T> {
+	
+	public Grid(int width, int height)
+	{
+		
+	}
+	
+	
+	private Cell<T> [][] _cells;
+}
