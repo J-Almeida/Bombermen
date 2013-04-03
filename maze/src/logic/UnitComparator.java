a6817081b4149caf25096f9ee8dcea8160f02228
Nay <dnpd.dd@gmail.com>
2013-04-03 18:39:12 +0100

    Fix a bug in UnitComparator (enables multiple dragons)

--- maze/src/logic/UnitComparator.java
+++ maze/src/logic/UnitComparator.java
@@ -2,19 +2,21 @@ package logic;
 
 import java.util.Comparator;
 
-public class UnitComparator implements Comparator<Unit> {
-
+public class UnitComparator implements Comparator<Unit>
+{
     @Override
-    public int compare(Unit arg0, Unit arg1) {
-    // TODO: Change this implementation to the one of EventComparator
-        if (arg0.IsSameType(arg1)) return 0;
+    public int compare(Unit arg0, Unit arg1)
+    {
+        if (arg0.IsSameType(arg1))
+            return arg0.GetId() - arg1.GetId();
 
         for (UnitType unt : UnitType.values())
         {
-            if (arg0.Is(unt)) return -1;
-            else if (arg1.Is(unt)) return 1;
+            if (arg0.Is(unt))
+                return -1;
+            else if (arg1.Is(unt))
+                return 1;
         }
         return 0;
     }
-
 }
