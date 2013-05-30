6556dd4bf0ead314ff49bb57718371af1635b416
Nay <dnpd.dd@gmail.com>
2013-05-30 01:32:40 +0100

    Initial barebone app (generated LibGDX Project Setup)

---
+++ bombermen2/bombermen-android/gen/pt/up/fe/pt/lpoo/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package pt.up.fe.pt.lpoo;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ bombermen2/bombermen-desktop/src/pt/up/fe/pt/lpoo/Main.java
@@ -0,0 +1,16 @@
+package pt.up.fe.pt.lpoo;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "bombermen";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new Bombermen(), cfg);
+	}
+}
