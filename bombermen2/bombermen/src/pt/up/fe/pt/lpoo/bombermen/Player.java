02418ad50a5d440a74aa89275393167fe86cd471
Nay <dnpd.dd@gmail.com>
2013-06-05 19:23:54 +0100

    Remove not required fields from client's Player

--- bombermen2/bombermen/src/pt/up/fe/pt/lpoo/bombermen/Player.java
+++ bombermen2/bombermen/src/pt/up/fe/pt/lpoo/bombermen/Player.java
@@ -48,61 +48,11 @@ public class Player extends Entity
     {
         super(Entity.TYPE_PLAYER, guid);
 
-        //_name = name;
-        //_currentBombs = 0;
-        //_maxBombs = Constants.INIT_NUM_MAX_BOMBS;
-        //_bombRadius = Constants.INIT_BOMB_RADIUS;
-        _speed = Constants.INIT_PLAYER_SPEED;
         _direction = Direction.SOUTH;
     }
 
-    //private String _name;
-    private int _currentBombs;
-    private int _maxBombs;
-    private int _bombRadius;
     private int _direction;
-    private float _speed;
     public static TextureRegion Regions[][];
-
-    public int GetCurrentBombs()
-    {
-        return _currentBombs;
-    }
-
-    public int GetMaxBombs()
-    {
-        return _maxBombs;
-    }
-
-    public float GetSpeed()
-    {
-        return _speed;
-    }
-
-    public int GetBombRadius()
-    {
-        return _bombRadius;
-    }
-
-    public void UpdateCurrentBombs(int inc)
-    {
-        _currentBombs += inc;
-    }
-
-    public void UpdateMaxBombs(int inc)
-    {
-        _maxBombs += inc;
-    }
-
-    public void UpdateBombRadius(int inc)
-    {
-        _bombRadius += inc;
-    }
-
-    public void SetBombRadius(int val)
-    {
-        _bombRadius = val;
-    }
 
     @Override
     public void draw(SpriteBatch batch, float parentAlpha)
