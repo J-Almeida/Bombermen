5dda3dd8f88753ea9bb7b6d5c4b2c886b0bd44a2
Nay <dnpd.dd@gmail.com>
2013-06-10 15:05:51 +0100

    Add applet project

---
+++ bombermen2/bombermen-applet/src/pt/up/fe/lpoo/bombermen/Main.java
@@ -0,0 +1,13 @@
+package pt.up.fe.lpoo.bombermen;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplet;
+
+
+public class Main extends LwjglApplet
+{
+    private static final long serialVersionUID = 1L;
+    public Main()
+    {
+        super(new Bombermen(), true);
+    }
+}
